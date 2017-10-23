a7d4ec2e4aca2b6f344bc28729f1ac7fb8859e74
mtr19920115 <mtr19920115@163.com>
2017-10-23 10:45:06 -0700

    Add files via upload

---
+++ BeerTrackObject.java
@@ -0,0 +1,76 @@
+public class BeerTrackObject
+{
+ private int desiredPSI;
+ private String color;
+ private String type;
+ private int EFD;
+ private int BeerID;
+ private int BottleDate;
+ private int currentPSI;
+ private String BeerName;
+ 
+ public void setdesiredPSI(int x)
+ {
+  desiredPSI=x;
+ }
+ public void setColor(String x)
+ {
+  color=x;
+ }
+ public void setType(String x)
+ {
+  type=x;
+ }
+ public void setEFD(int x)
+ {
+  EFD=x;
+ }
+ public void setBeerID(int x)
+ {
+  BeerID=x;
+ }
+ public void setBottleDate(int x)
+ {
+  BottleDate=x;
+ }
+ public void setCurrentPSI(int x)
+ {
+  currentPSI=x;
+ }
+ public void setBeerName(String x)
+ {
+  BeerName=x;
+ }
+ public int getdesiredPSI()
+ {
+  return desiredPSI;
+ }
+ public String getColor()
+ {
+  return color;
+ }
+ public String getType()
+ {
+  return type;
+ }
+ public int getEFD()
+ {
+  return EFD;
+ }
+ public int getBeerID()
+ {
+  return BeerID;
+ }
+ public int getBottleDate()
+ {
+  return BottleDate;
+ }
+ public int getCurrentPSI()
+ {
+  return currentPSI;
+ }
+ public String BeerName()
+ {
+  return BeerName;
+ }
+}         
