34296fd29bd31808eb729af01400b91e55cdeb16
bhawley84 <brendon.hawley.55@my.csun.edu>
2017-10-27 19:53:14 -0700

    fixing bs

---
+++ testing/BeerArray.java
@@ -0,0 +1,80 @@
+<<<<<<< HEAD
+public class BeerArray{
+    List<Beer> beerArray = new ArrayList<Beer>();
+    Beer lager, paleAle, amberAle, brownAle, stout, porter, belgianWhite;
+lager.setDesiredPSI(17);
+lager.setDesiredTemp(45);
+lager.setType("Lager")
+        lager.setColor();
+paleAle.setDesiredPSI(16);
+paleAle.setDesiredTemp(45);
+paleAle.setType("Pale Ale")
+        paleAle.setColor();
+amberAle.setDesiredPSI(16);
+amberAle.setDesiredTemp(50);
+amberAle.setType("Amber Ale")
+        amberAle.setColor();
+brownAle.setDesiredPSI(11);
+brownAle.setDesiredTemp(40);
+brownAle.setType("Brown Ale")
+        brownAle.setColor();
+stout.setDesiredPSI(11);
+stout.setDesiredTemp(55);
+stout.setType("Stout")
+        stout.setColor();
+porter.setDesiredPSI(6);
+porter.setDesiredTemp(50);
+porter.setType("Porter")
+        porter.setColor();
+belgianWhite.setDesiredPSI(9);
+belgianWhite.setDesiredTemp(45);
+belgianWhite.setType("Belgian White")
+        belgianWhite.setColor();
+beerArray.add(lager);
+beerArray.add(paleAle);
+beerArray.add(amberAle);
+beerArray.add(brownAle);
+beerArray.add(stout);
+beerArray.add(porter);
+beerArray.add(belgianWhite);
+=======
+public class BeerArray{
+    List<Beer> beerArray = new ArrayList<Beer>();
+    Beer lager, paleAle, amberAle, brownAle, stout, porter, belgianWhite;
+lager.setDesiredPSI(17);
+lager.setDesiredTemp(45);
+lager.setType("Lager")
+        lager.setColor();
+paleAle.setDesiredPSI(16);
+paleAle.setDesiredTemp(45);
+paleAle.setType("Pale Ale")
+        paleAle.setColor();
+amberAle.setDesiredPSI(16);
+amberAle.setDesiredTemp(50);
+amberAle.setType("Amber Ale")
+        amberAle.setColor();
+brownAle.setDesiredPSI(11);
+brownAle.setDesiredTemp(40);
+brownAle.setType("Brown Ale")
+        brownAle.setColor();
+stout.setDesiredPSI(11);
+stout.setDesiredTemp(55);
+stout.setType("Stout")
+        stout.setColor();
+porter.setDesiredPSI(6);
+porter.setDesiredTemp(50);
+porter.setType("Porter")
+        porter.setColor();
+belgianWhite.setDesiredPSI(9);
+belgianWhite.setDesiredTemp(45);
+belgianWhite.setType("Belgian White")
+        belgianWhite.setColor();
+beerArray.add(lager);
+beerArray.add(paleAle);
+beerArray.add(amberAle);
+beerArray.add(brownAle);
+beerArray.add(stout);
+beerArray.add(porter);
+beerArray.add(belgianWhite);
+>>>>>>> 5df23d0e8156b1d1551f25ea157c71e95d398186
+}
