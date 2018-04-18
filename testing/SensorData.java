dff4eb08e371d01de3b0d08f943c672d13710058
bhawley84 <brendon.hawley.55@my.csun.edu>
2018-04-18 16:30:02 -0700

    updated to get data for 3 beers
    use renewSensorData to fetch newest data from web;
    use getTemp(int beerIndex) and
    getPress(int beerIndex)
    0 is real beer, 1 and 2 are sligtly off of 0

--- testing/SensorData.java
+++ testing/SensorData.java
@@ -5,28 +5,28 @@ import java.net.*;
 import java.io.*;
 
 public class SensorData{
-
-
-    String inputLine;
-    double temp = 0.0;
-    double press = 0.0;
-    public SensorData() throws Exception {
+    private SensorData(){}
+    private double[] temp = new double[3];
+    private double[] press = new double[3];
+    public void renewSensorData() throws Exception {
+        int i = 0;
         URL input = new URL ("http://192.168.2.8/");
         BufferedReader in = new BufferedReader(new InputStreamReader(input.openStream()));
+        String inputLine;
         while ((inputLine = in.readLine()) != null){
-            String[] parts =inputLine.split(" ");
-            temp = Double.parseDouble(parts[0]);
-            press = Double.parseDouble(parts[1]);
+            String[] parts = inputLine.split(" ");
+            temp[i] = Double.parseDouble(parts[0]);
+            press[i] = Double.parseDouble(parts[1]);
+            i++;
         }
         in.close();
     }
 
-    public double getTemp() {
-        return temp;
+    public double getTemp(int index) {
+        return temp[index];
     }
 
-    public double getPress() {
-        return press;
+    public double getPress(int index) {
+        return press[index];
     }
-
 }
